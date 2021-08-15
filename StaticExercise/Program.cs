368d50eb246c53314947b94cd449c8a21db8b47f
mrLeeDevelops <mrlee.truecoder@gmail.com>
2021-08-14 23:08:10 -0500

    Completed Static Exercise #1

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var farenheit = TempConverter.CelciusToFarenheit(37);
+            var celcius = TempConverter.FarenheitToCelcius(212);
+            Console.WriteLine($"The Farenheit Temperature Converted From Celcius is {farenheit} Degrees F.");
+            Console.WriteLine($"The Celcius Temperature Converted From Farenheit is {celcius} Degrees C.");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelcius(double CTemp)
+        {
+            return (CTemp - 32) * 5 / 9;
+        }
+
+        public static double CelciusToFarenheit(double FTemp)
+        {
+            return ((FTemp * 9) / 5) + 32;
+        }
+    }
+
+}
